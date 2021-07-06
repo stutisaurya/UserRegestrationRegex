cd5a3bc058cc2ff9e2e23ecab965784df50951e9
stutisaurya <stuti.saurya@gmail.com>
2021-07-06 22:09:21 +0530

    Added:ValidateFirstName

---
+++ src/com/UserRegistration.java
@@ -0,0 +1,31 @@
+package com;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+	public static boolean isFirstName(String firstname) {
+		String regex = "^[A-Z]{1}[a-z]{2,}";
+		Pattern patt = Pattern.compile(regex);
+		if (firstname == null) {
+			return false;
+		}
+		Matcher match = patt.matcher(firstname);
+		return match.matches();
+	}
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter firstname:");
+		String firstname = input.nextLine();
+		if (isFirstName(firstname) == true) {
+			System.out.println("Firstname is Correct");
+		} else {
+			System.out.println("Firstname is Incorrect " + "\n"
+					+ "First name starts with Capital letter and has minimum 3 characters");
+		}
+	}
+
+}
